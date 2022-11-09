f46d5d69fb71b255f8259c3a1a4c9fab1027b9b3
akmann <akmann.henry@gmail.com>
2022-11-09 14:22:43 +0100

    minor change

--- parsers/grepolis_market.js
+++ parsers/grepolis_market.js
@@ -65,7 +65,7 @@ export default async function (job, data, file = false) {
 						{
 							title: "Market emptied",
 							description: sea,
-							footer: { text: new Date().toLocaleString("de") },
+							footer: { text: new Date().toLocaleString("de-DE") },
 							color: urgent ? "16711680" : null,
 							fields
 						}
