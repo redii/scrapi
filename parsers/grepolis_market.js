c4f4d033ced295dccb8ec7c230dc92c4cb44d1ac
akmann <akmann.henry@gmail.com>
2022-11-09 14:10:04 +0100

    a lot

--- parsers/grepolis_market.js
+++ parsers/grepolis_market.js
@@ -6,12 +6,21 @@ const threshold = {
 	urgent: 20000
 }
 
-const discordUrl =
-	"https://discord.com/api/webhooks/1039886335693041764/JeeSiIN9helfO12Vsw6SgKrRZeu8DXyEM6ArZCUxTuPxEqzxgGNNnjtndLK9SsT0kBYi"
+// prettier-ignore
+const discordUrls = {
+	"de135": {
+		"sea44": "https://discord.com/api/webhooks/1039888859690975294/V21S-P0jdL82siOidhUiWyeyrdx5968B_a5rEvqQzAthQkwI9OO7N29ybjIZ_lyzAT4L",
+		"sea45": "https://discord.com/api/webhooks/1039888945581924393/_7f9d_z1nSJUHiBWk7BRMx2Xz6ZBtp7cnNHd-1GjWC2tHA9aRw2LpGEx2FYTIzi3YOHy",
+		"sea54": "https://discord.com/api/webhooks/1039886335693041764/JeeSiIN9helfO12Vsw6SgKrRZeu8DXyEM6ArZCUxTuPxEqzxgGNNnjtndLK9SsT0kBYi",
+		"sea55": "https://discord.com/api/webhooks/1039888658012053564/dW2CazgEDYyQgbiTqEQ6-K4pODM0bcdKv4NR5kTfhgH7WLjkRc2VtCIfwQi4YvV-NKuW"
+	}
+}
 
 export default async function (job, data, file = false) {
 	try {
 		const json = JSON.parse(data).json
+		const world = job.name.split("/")[1]
+		const sea = job.name.split("/")[3]
 
 		// check for emptied market
 		let urgent = false
@@ -49,13 +58,13 @@ export default async function (job, data, file = false) {
 
 		// send messade to discord webhooks
 		if (fields.length) {
-			got.post(discordUrl, {
+			got.post(discordUrls[world][sea], {
 				json: {
 					content: "@everyone",
 					embeds: [
 						{
 							title: "Market emptied",
-							description: job.name.split("/")[3],
+							description: sea,
 							footer: { text: new Date().toLocaleString("de") },
 							color: urgent ? "16711680" : null,
 							fields
